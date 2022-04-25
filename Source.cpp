b97a88cae11090bee280caca42123b4ce68a9bf1
noonenever7@gmail.com <noonenever7@gmail.com>
2022-04-25 19:15:40 +0700

    Release 1.2 Final

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,18 @@ void inputStr(char** pstr, char str[][COL]) {
 	cout << endl;
 }
 
+int secondNum(char** pstr, int indexStr) {
+	int num2 = 0, j = 0;
+	while (pstr[indexStr][j] != '\0') {
+		if (pstr[indexStr][j] >= 48 && pstr[indexStr][j] <= 57) num2++;
+		if (num2 == 2) {
+			return pstr[indexStr][j];
+		}
+		j++;
+	}
+	return -1;
+}
+
 int countNum(char** pstr, int indexStr) {
 	int nNum = 0, j = 0;
 	while (pstr[indexStr][j] != '\0') {
@@ -29,10 +41,17 @@ int countNum(char** pstr, int indexStr) {
 }
 
 void outStr(char** pstr) {
+	int num2 = 0;
 	cout << "\tRESULT:" << endl;
 	for (int i = 0; i < ROW; i++) {
 		cout << i + 1 << " | " << pstr[i] << endl;
-		cout << "  |" << setw(40) << "Num of digits: " << countNum(pstr, i) << endl;
+		cout << "  |" << setw(30) << "Num of digits: " << countNum(pstr, i);
+		num2 = secondNum(pstr, i);
+		if (num2 >= 0) {
+			cout << "\tSecond num: " << num2 - 48 << endl;
+		}
+		else
+			cout << "\tNo numbers (or missing 2nd element) in the string" << endl;
 	}
 }
 
